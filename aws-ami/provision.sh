fceaf658c4f87f92ab6cd38165b0f6dce8aab677
Raul Sevilla <rsevilla@redhat.com>
2021-01-14 01:11:51 +0100

    Add tox
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- aws-ami/provision.sh
+++ aws-ami/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo dnf update -y
-sudo dnf install -y podman vim golang bind-utils python3 python3-pip python3-elasticsearch python3-virtualenv figlet dos2unix jq git parallel make ansible parallel bc
+sudo dnf install -y podman vim golang bind-utils python3 python3-pip python3-elasticsearch python3-virtualenv python3-tox figlet dos2unix jq git make ansible parallel bc tmux
 sudo dnf groupinstall -y "Development Tools"
 sudo pip install awscli
