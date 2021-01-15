9935e4cdef12fa6541dbabff34f96e9e53fe096c
Raul Sevilla <rsevilla@redhat.com>
2021-01-15 11:44:40 +0100

    Add bash completion
    Signed-off-by: Raul Sevilla <rsevilla@redhat.com>

--- aws-ami/provision.sh
+++ aws-ami/provision.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo dnf update -y
-sudo dnf install -y podman vim golang bind-utils python3 python3-pip python3-elasticsearch python3-certifi python3-virtualenv python3-tox figlet dos2unix jq git make ansible parallel bc tmux
+sudo dnf install -y podman vim golang bind-utils python3 python3-pip python3-elasticsearch python3-certifi python3-virtualenv python3-tox figlet dos2unix jq git make ansible parallel bc tmux bash-completion
 sudo dnf groupinstall -y "Development Tools"
 sudo pip install awscli
